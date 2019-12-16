37815ef5d5af3b384bdca2b2dfd1832291518469
raghuram-katarakonda <45900662+raghuram-katarakonda@users.noreply.github.com>
2019-12-16 11:36:33 +0530

    Initial commit

---
+++ src/main/java/com/aws/lambda/S3TriggerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aws.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3TriggerSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3TriggerSpringbootApplication.class, args);
+	}
+
+}
